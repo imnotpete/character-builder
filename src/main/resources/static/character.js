96d57e2dbafa9b39dba53370a287abea27314816
Sam Jones <sam@createdifferent.com>
2017-04-30 20:42:13 -0400

    added condition/hp/damage calculation

--- src/main/resources/static/character.js
+++ src/main/resources/static/character.js
@@ -38,7 +38,61 @@
 	}
 	
 	function recalculateHp() {
+		var conMod = parseInt($('#conMod').val()) || 0;
+		var hpTemp = parseInt($('#hpTemp').val()) || 0;
+		var hpTotal = conMod + hpTemp;
+		var damage = parseInt($('#damage').val()) || 0;
+		var hpCurrent = hpTotal - damage;
+		var hpPercent = (hpCurrent < 1 ? 0 : Math.trunc(hpCurrent / hpTotal * 100));
+		var nonlethal = parseInt($('#nonlethal').val()) || 0;
 		
+		$('#hpTotal').val(hpTotal);
+		$('#hpCurrent').val(hpCurrent);
+		
+		setProgressBar(hpPercent);
+		setCondition(hpCurrent, nonlethal);
+	}
+	
+	function setProgressBar(hpPercent) {
+		var progressClasses = "progress-bar progress-bar-success";
+		if (hpPercent < 25) {
+			progressClasses = "progress-bar progress-bar-danger";
+		} else if (hpPercent < 50) {
+			progressClasses = "progress-bar progress-bar-warning";
+		}
+
+		var progressElement = $('#hpProgress'); 
+		progressElement.css('width', hpPercent + "%");
+		progressElement.text(hpPercent + "%");
+		progressElement.attr('class', progressClasses);
+	}
+	
+	function setCondition(hpCurrent, nonlethal) {
+		var condition = "Alive";
+		var conditionClasses = "label label-success";
+		
+		if (hpCurrent > 0) {
+			if (hpCurrent == nonlethal) {
+				condition = "Staggered";
+				conditionClasses = "label label-warning";
+			} else if (hpCurrent < nonlethal) {
+				condition = "Unconscious";
+				conditionClasses = "label label-warning";
+			}
+		} else if (hpCurrent == 0) {
+			condition = "Disabled";
+			conditionClasses = "label label-warning";
+		} else if (hpCurrent <= -10) {
+			condition = "Dead";
+			conditionClasses = "label label-danger";
+		} else if (hpCurrent < 0) {
+			condition = "Dying";
+			conditionClasses = "label label-danger";
+		}
+		
+		var element = $('#condition');
+		element.text(condition);
+		element.attr('class', conditionClasses);
 	}
 	
 	function recalculateAttacks() {
