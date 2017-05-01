6227284741b3e5afac96b160d9e5e0876a3c6ba0
Sam Jones <sam@createdifferent.com>
2017-04-30 22:12:10 -0400

    added distinct css file; made "add attack" button work

--- src/main/resources/static/js/character.js
+++ src/main/resources/static/js/character.js
@@ -2,6 +2,17 @@ $(document).ready(setup);
 
 function setup() {
 	$('input').change(recalculateAll);
-
+	$('#addAttackButton').click(addAttack);
 	recalculateAll();
+}
+
+function addAttack() {
+	var attacks = $('#attacks');
+	var newAttack = $('#attackTemplate').clone();
+	attacks.append(newAttack);
+	newAttack.show();
+}
+
+function deleteAttack() {
+	
 }
