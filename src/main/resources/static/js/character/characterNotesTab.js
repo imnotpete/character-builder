e61dcfb70b738a1c91e02037a2f9ac6417f2f411
Sam Jones <sam@imnotpete.com>
2017-08-12 02:50:49 -0400

    gh-24 -- using mmd.js to process markdown on notes tab

--- src/main/resources/static/js/character/characterNotesTab.js
+++ src/main/resources/static/js/character/characterNotesTab.js
@@ -3,5 +3,14 @@ function setupNotesTab(self, data) {
 		data = {}
 	}
 	
+	self.editingNotes = ko.observable(false);
 	self.notes = ko.observable(data.notes);
+	
+	self.notesRendered = ko.computed(function() {
+		return mmd(self.notes());
+	});
+	
+	self.toggleEditingNotes = function() {
+		self.editingNotes(! self.editingNotes());
+	}
 }
