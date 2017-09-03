eca4db7c8e54d500682dbe37b71e515f484ceec3
Sam Jones <sam@imnotpete.com>
2017-09-03 09:47:02 -0400

    removed unused dependencies

--- src/main/java/com/imnotpete/rpg/web/TestController.java
+++ src/main/java/com/imnotpete/rpg/web/TestController.java
@@ -1,10 +1,8 @@
 package com.imnotpete.rpg.web;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 //@RestController
 public class TestController {
